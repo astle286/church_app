3b98975fdff673ba8122bb6d3ebae0a587760b6c
astle286 <astle257@gmail.com>
2025-09-22 20:40:03 +0530

    attempt9

--- app/__init__.py
+++ app/__init__.py
@@ -17,5 +17,12 @@ def create_app():
 
     from .routes import main
     app.register_blueprint(main)
-
+    
     return app
+
+from flask import Flask
+from app.models import db
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
