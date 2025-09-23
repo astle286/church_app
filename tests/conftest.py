c0d71979734f22f33f7a847cc3eca31ae72a08e4
astle286 <astle257@gmail.com>
2025-09-23 12:49:23 +0530

    FIRST TEST3

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from app import create_app, db
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+        "WTF_CSRF_ENABLED": False
+    })
+    with app.app_context():
+        db.create_all()
+        yield app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
