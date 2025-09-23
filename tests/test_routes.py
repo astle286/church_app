ecb52f23b2f0e28fb8dec780ddc7cdd42fa3aad3
astle286 <astle257@gmail.com>
2025-09-23 13:13:00 +0530

    FIRST TEST6

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,8 @@
-def test_families_page(client):
-    response = client.get('/families')  # adjust if your route is different
+import sys
+sys.path.append("/app")
+
+def test_index_page(client):
+    response = client.get('/')
     assert response.status_code == 200
     assert b"Families" in response.data  # checks if the page contains the title
+    assert b"Add Family" in response.data  # checks for a key button/link
